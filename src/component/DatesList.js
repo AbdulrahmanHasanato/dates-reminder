95077b2778ec5b03b141da73962f9058993d14bc
Abdulrahman <byjyta@gmail.com>
2023-12-21 13:32:01 +0300

    initial upload

---
+++ src/component/DatesList.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+
+const DatesList = ({ person }) => {
+    return (
+        <Row className="justify-content-center">
+            <Col sm="8" className="">
+                <div className="rectangle p-2">
+
+                    {person.length ? (person.map((item) => {
+                        return (
+                            <div key={item.id} className="d-flex border-bottom mx-3 my-2">
+                                <img src={item.img} className="img-avatar" alt="Profile picture" />
+                                <div className="px-3">
+                                    <p className="d-inline fs-5">{item.name}</p>
+                                    <p className="fs-6">{item.date}</p>
+                                </div>
+                            </div>
+                        )
+                    })) : <h2 className="p-5 text-center">You don't have any reminders yet</h2>}
+
+                </div>
+            </Col>
+        </Row>
+    )
+}
+
+export default DatesList
---
+++ src/data.js
@@ -0,0 +1,8 @@
+export const person = [
+    { id: "1", name: "Muhammad", date: "5 PM", img: "img6.jpg" },
+    { id: "2", name: "Ahmad", date: "4 PM", img: "img2.jpg" },
+    { id: "3", name: "Muhannad", date: "3 PM", img: "img3.jpg" },
+    { id: "4", name: "Yaman", date: "2 PM", img: "img4.jpg" },
+    { id: "5", name: "Zahiya", date: "1 PM", img: "img5.jpg" },
+    { id: "6", name: "Abdulrahman", date: "11 AM", img: "img1.jpg" },
+]
